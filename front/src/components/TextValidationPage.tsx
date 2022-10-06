af30ff180f1ae50bb001d73050b95b133141b9f9
Elena Bakova <lenabakova72@gmail.com>
2022-10-06 21:20:58 +0300

    Moved upload to the TextValidationPage.tsx

--- front/src/components/TextValidationPage.tsx
+++ front/src/components/TextValidationPage.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import {DropZone} from "./DropZone";
 import {FileList} from "./FileList";
-import Upload from "./Upload";
+import Button from "@mui/material/Button";
 
+const AcceptedFileType = {Doc: '.docx'};
 
 export const TextValidationPage = React.memo(() => {
     const [isDropActive, setIsDropActive] = React.useState(false)
     const [files, setFiles] = React.useState<File[]>([])
+    const fileRef = React.useRef<HTMLInputElement>(null);
+
+    // File formats that can be uploaded
+    const acceptedFormats = AcceptedFileType.Doc;
+
+    const handleFileSelect = (event: any) => {
+        setFiles([event?.target?.files?.[0]] as File[]);
+    };
 
     const onDragStateChange = (dragActive: boolean) => {
         setIsDropActive(dragActive)
@@ -20,7 +29,14 @@ export const TextValidationPage = React.memo(() => {
         <div style={{alignContent: "space-around", justifyItems: "center"}}>
             <DropZone onDragStateChange={onDragStateChange} onFilesDrop={onFilesDrop}>
                 <h2>Перетащите файлы сюда</h2>
-                <Upload/>
+                <Button
+                    variant="contained"
+                    component="label"
+                    style={{textTransform: 'none', fontSize: 'medium'}}
+                >
+                    или нажмите для загрузки
+                    <input ref={fileRef} hidden type="file" accept={acceptedFormats} onChange={handleFileSelect}/>
+                </Button>
                 <FileList files={files}/>
             </DropZone>
         </div>
