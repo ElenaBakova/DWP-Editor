86837a1a0f0feb7c5ebb700b0bd19e38e3a06934
Elena Bakova <lenabakova72@gmail.com>
2023-03-07 15:59:03 +0300

    Added toolbar buttons

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import {TopBar} from "./components/TopBar";
 import {TextValidationPage} from "./components/TextValidationPage";
-import {Grid} from "@mui/material";
+import {Grid, Switch} from "@mui/material";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
     return (
         <Grid container direction="column">
-            <Grid container item>
+            {/*<Grid container item>
                 <TopBar/>
             </Grid>
 
             <Grid item>
                 <TextValidationPage/>
-            </Grid>
+            </Grid>*/}
+            <Routes>
+                <TopBar/>
+                    <Route path="/" element={<TextValidationPage/>} />
+            </Routes>
         </Grid>
     );
 }
--- front/src/components/TopBar.tsx
+++ front/src/components/TopBar.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import {AppBar, Box, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Toolbar, Typography, Container, Button, Divider} from "@mui/material";
 
 export function TopBar() {
     return (
         <Box sx={{flexGrow: 1}}>
             <AppBar position="static" color="primary" enableColorOnDark>
-                <Toolbar>
-                    <Typography variant="h6" component="div">
-                        Онлайн редактор РПД
-                    </Typography>
-                </Toolbar>
+                <Container maxWidth="xl" >
+                    <Toolbar disableGutters>
+                        <Typography variant="h6" component="div" sx={{display: {xs: "none", md: "flex"}, marginRight: 3}}>
+                            Онлайн редактор РПД
+                        </Typography>
+
+                        {/*<Box sx={{flexGrow: 1}}>*/}
+                        <Button
+                            // onClick={handleCloseNavMenu}
+                            size="large"
+                            sx={{color: "white", display: "inline-block", alignSelf:"center"}}
+                        >
+                            {'Проверить'}
+                        </Button>
+                        <Divider orientation="vertical" variant="middle" flexItem />
+                        <Button
+                            // onClick={handleCloseNavMenu}
+                            size="large"
+                            sx={{color: "white", display: "inline-block"}}
+                        >
+                            {'Редактировать'}
+                        </Button>
+                        {/*</Box>*/}
+                    </Toolbar>
+                </Container>
             </AppBar>
         </Box>
     );
